6f6ba01261993ca17c187b40ac72305712b667db
최승언 <105850728+dmskk@users.noreply.github.com>
2024-07-31 21:40:08 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1464 ms, Memory: 58216 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,29 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		try {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.valueOf(st.nextToken());
-			int m = Integer.valueOf(st.nextToken());
-			
-			st = new StringTokenizer(br.readLine());
-			int[] sum = new int[n+1];
-			sum[0] = 0;
-			for(int i=1; i<=n; i++) {
-				sum[i] = sum[i-1] + Integer.valueOf(st.nextToken());
-			}
-			
-			for(int i=0; i<m; i++) {
-				st = new StringTokenizer(br.readLine());
-				System.out.println(0-sum[Integer.valueOf(st.nextToken())-1]+sum[Integer.valueOf(st.nextToken())]);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			br.close();			
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int[] d = new int[n+1];
+            for(int i=1; i<=n; i++) {
+                d[i] = d[i-1] + Integer.parseInt(st.nextToken());
+            }
+
+            for(int i=0; i<m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int I = Integer.parseInt(st.nextToken());
+                int J = Integer.parseInt(st.nextToken());
+                int result = d[J] - d[I-1];
+                System.out.println(result);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            br.close();
+        }
+    }
+
+
 }
